7d59c4b39fca9651e7e6cd1b7cc33b7af8acc76a
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2022-10-08 15:54:10 -0600

    LOGIN FINISH

---
+++ app/src/main/java/cr/una/example/frontend_farmastock/service/LoginService.kt
@@ -0,0 +1,29 @@
+package cr.una.example.frontend_farmastock.service
+
+
+import cr.una.example.frontend_farmastock.model.LoginRequest
+import cr.una.example.frontend_farmastock.model.UserLoginResponse
+import cr.una.example.frontend_farmastock.model.UserResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface LoginService {
+    @POST("/v1/users/login")
+    suspend fun login(@Body userLogin: LoginRequest) : Response<UserLoginResponse>
+
+    @GET("v1/users/email/{username}")
+    suspend fun getUserByEmail(@Path("username") username: String) : Response<UserResponse>
+
+    companion object {
+        private var loginService : LoginService? = null
+        fun getInstance() : LoginService {
+            if (loginService == null) {
+                loginService = ServiceBuilder.buildService(LoginService::class.java)
+            }
+            return loginService!!
+        }
+    }
+}
