82b58bf7ed20f80b202fff6969e64f4b39cedcc0
JohanBlanco1099 <102931533+JohanBlanco1099@users.noreply.github.com>
2022-10-26 15:33:41 -0600

    Reminder Main Fragment Created

---
+++ app/src/main/java/cr/una/example/frontend_farmastock/adapter/ReminderAdapter.kt
@@ -0,0 +1,89 @@
+package cr.una.example.frontend_farmastock.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import cr.una.example.frontend_farmastock.R
+import cr.una.example.frontend_farmastock.databinding.ReminderItemBinding
+import cr.una.example.frontend_farmastock.model.ReminderResponse
+
+class ReminderAdapter: RecyclerView.Adapter<ReminderAdapter.MainViewHolder>(),
+    Filterable {
+    var reminderFilterList = mutableListOf<ReminderResponse>()
+    private var reminderList = mutableListOf<ReminderResponse>()
+
+    fun setReminderList(reminderResponseList: List<ReminderResponse>) {
+        this.reminderList.clear()
+        this.reminderList.addAll(reminderResponseList.toMutableList())
+        this.reminderFilterList.addAll(reminderList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ReminderItemBinding.inflate(inflater, parent, false)
+        return MainViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        val reminder = reminderFilterList[position]
+        holder.binding.DescriptionValue.text = reminder.description
+        holder.binding.DateTimeValue.text = reminder.createDate.toString()
+
+        holder.itemView.setOnClickListener() {
+            val bundle = bundleOf(REMINDER_ID to reminderFilterList[position].id.toString())
+
+//            holder.itemView.findNavController().navigate(
+////                R.id.action_reminderMainFragment_to_reminderDetailsFragment, bundle
+//            )
+        }
+
+    }
+
+    // NUEVO
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+                if (!charSearch.isEmpty()) {
+                    val resultList = mutableListOf<ReminderResponse>()
+                    for (row in reminderList) {
+                        if (row.description?.lowercase()?.contains(charSearch.lowercase()) == true || row.createDate?.toString()?.contains(charSearch) == true)
+                        {
+                            resultList.add(row)
+                        }
+                    }
+                    reminderFilterList= resultList
+                }else{
+                    reminderFilterList = reminderList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = reminderFilterList
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                reminderFilterList = results?.values as MutableList<ReminderResponse>
+                notifyDataSetChanged()
+            }
+
+        }
+    }
+    //
+
+    override fun getItemCount(): Int {
+        return reminderFilterList.size
+    }
+
+    companion object {
+        const val REMINDER_ID = "reminder_id"
+    }
+    class MainViewHolder(
+        val binding: ReminderItemBinding
+    ) : RecyclerView.ViewHolder(binding.root)
+}
--- app/src/main/java/cr/una/example/frontend_farmastock/view/ReminderMainFragment.kt
+++ app/src/main/java/cr/una/example/frontend_farmastock/view/ReminderMainFragment.kt
@@ -5,22 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import cr.una.example.frontend_farmastock.R
+import cr.una.example.frontend_farmastock.adapter.ReminderAdapter
 import cr.una.example.frontend_farmastock.databinding.FragmentMedicineMainBinding
-import cr.una.example.frontend_farmastock.viewmodel.MedicineViewModel
-import cr.una.example.frontend_farmastock.viewmodel.MedicineViewModelFactory
-import cr.una.example.frontend_farmastock.viewmodel.ReminderViewModel
-import cr.una.example.frontend_farmastock.viewmodel.ReminderViewModelFactory
+import cr.una.example.frontend_farmastock.databinding.FragmentReminderMainBinding
+import cr.una.example.frontend_farmastock.viewmodel.*
 
 
 class ReminderMainFragment : Fragment() {
 
     // Definition of the binding variable
-    private var _binding: ReminderMainFragment? = null
+    private var _binding: FragmentReminderMainBinding? = null
     private val binding get() = _binding!!
 
-    private val reminderViewModel: ReminderViewModel by activityViewModels {
+
+    private val reminderViewModel: ReminderViewModel by activityViewModels{
         ReminderViewModelFactory()
     }
 
@@ -28,10 +31,52 @@ class ReminderMainFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reminder_main, container, false)
+    ): View {
+        val adapter = ReminderAdapter()
+        _binding = FragmentReminderMainBinding.inflate(inflater, container, false)
+        binding.reminderList.adapter = adapter
+        binding.reminderList.layoutManager = LinearLayoutManager(activity)
+        reminderViewModel.findAllReminders()
+        reminderViewModel.state.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                // just checking equality because Loading is a -singleton object instance-
+                StateReminder.Loading -> {
+                    // TODO: If you need do something in loading
+                }
+                // Error and Success are both -classes- so we need to check their type with 'is'
+                is StateReminder.Error -> {
+                    // TODO: If you need do something in error
+                }
+                is StateReminder.SuccessList -> {
+                    state.reminderList?.let { adapter.setReminderList(it) }
+                }
+                else -> {
+                    // TODO: Not state loaded
+                }
+            }
+        }
+        // NUEVO
+        binding.SearchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filter.filter(newText)
+                return false
+            }
+
+        })
+
+        return binding.root
+
     }
-
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentReminderMainBinding.bind(view)
+        binding.buttonMainAddReminder.setOnClickListener{
+//            Navigation.findNavController(view).navigate(R.id.action_reminderMainFragment_to_reminderAddFragment)
+        }
+    }
 }
