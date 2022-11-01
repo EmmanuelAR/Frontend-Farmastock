367a7802489ae76d964c2a63388a1e73d7f8cf01
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2022-11-01 15:30:57 -0600

    alarm test, se tiene que arreglar dependecia con notify

---
+++ app/src/main/java/cr/una/example/frontend_farmastock/utils/Constants.kt
@@ -0,0 +1,8 @@
+package cr.una.example.frontend_farmastock.utils
+
+object Constants {
+    const val EXTRA_EXACT_ALARM_TIME = "EXTRA_EXACT_ALARM_TIME"
+
+    const val ACTION_SET_EXACT = "ACTION_SET_EXTRA"
+    const val ACTION_SET_REPETITIVE_EXACT = "ACTION_SET_REPETITIVE_EXACT"
+}
